e4b1f0f9c30811f930d2ada660ca7dca157c4cdb
simonfan <simonxd@gmail.com>
2014-05-27 19:16:29 -0300

    working on tests

---
+++ src/__pump/streams.js
@@ -0,0 +1,70 @@
+define(function (require, exports, module) {
+
+
+	var _ = require('lodash'),
+		q = require('q');
+
+
+	function execPipeStream(action, pipeIds) {
+
+		// pipes that should run the action
+		var pipes;
+
+		// pick pipes if pipeIds was defined
+		if (pipeIds) {
+			// pipeIds must be an array
+			pipeIds = _.isArray(pipeIds) ? pipeIds : [pipeIds];
+
+			// pick
+			pipes = _.pick(this.pipes, pipeIds);
+
+		} else {
+			// pipes are all pipes available.
+			pipes = this.pipes;
+		}
+
+		// invoke pump on all pipes and return the promise
+		var results = _.map(pipes, function (pipe) {
+
+			return pipe[action]();
+		});
+
+		return q.all(results);
+
+	}
+
+	exports.pump = _.partial(execPipeStream, 'pump');
+
+	exports.drain = _.partial(execPipeStream, 'drain');
+
+	exports.inject = function inject(data, pipeIds) {
+
+
+		var set = this.srcSet || this.set;
+
+
+		// [0] throw error if there is no source in the pump object.
+		if (!this.source) {
+			throw new Error('No source for pump');
+		}
+
+		// [1] SET all data onto the SOURCE
+		var srcSetRes = _.map(data, function (value, key) {
+
+			return set.call(this, this.source, key, value);
+
+		}, this);
+
+
+		// [2] wait for all srcSets
+		return q.all(srcSetRes)
+			// [2.1] then invoke pump on success
+			//       wrap in a method in order to guarantee
+			//       pump is invoked with NO ARGUMENTS
+			.then(_.bind(function() { this.pump(pipeIds); }, this))
+			// [2.2] or throw error
+			.fail(function (e) { throw e; });
+	};
+
+
+});
--- test/basics.js
+++ test/basics.js
@@ -16,17 +16,120 @@
 		define(deps, factory);
 	}
 
-})('test', function(Pump, should) {
+})('test', function(pump, should) {
 	'use strict';
 
-	describe('Pump basics', function () {
-		beforeEach(function (done) {
-			done();
+	describe('pump basics', function () {
+		beforeEach(function () {
+
+			this.source = {
+				key1: 'v1',
+				key2: 'v2',
+				key3: 'v3',
+				key4: 'v4',
+			};
+
+			var dataPump = pump.extend();
+
+			this.dpump = dataPump(this.source);
+
 		});
 
-		it('is fine (:', function () {
-			var fruit = { name: 'banana' }
-			fruit.should.have.property('name', 'banana');
+		it('pump', function (testdone) {
+
+			var source = this.source;
+
+			var d1 = {},
+				d2 = {},
+				d3 = {},
+				d4 = {};
+
+
+			var dpump = this.dpump;
+
+
+			var pipes = {};
+
+			// create pipes
+			pipes.pid1 = dpump.pipe('pid1', {
+				key1: 'dest1K1',
+				key4: 'dest1K4',
+			})
+			.to(d1);
+
+			pipes.pid2 = dpump.pipe('pid2', {
+				key1: 'dest2K1',
+				key3: 'dest2K3'
+			})
+			.to(d2);
+
+			// pump 1
+			dpump
+				.pump()
+				.then(function () {
+
+					// tests
+					d1['dest1K1'].should.eql('v1');
+					should(d1['dest1K2']).be.undefined;
+					should(d1['dest1K3']).be.undefined;
+					d1['dest1K4'].should.eql('v4');
+
+
+				})
+				.then(function () {
+
+					// remove one pipe
+					dpump.unpipe('pid1');
+
+					// change values on source
+					source.key1 = 'altered-v1';
+					source.key4 = 'altered-v4';
+
+					// create more pipes
+					pipes.pid3 = dpump.pipe('pid3', {
+						key1: 'dest3K1',
+						key2: 'dest3K2',
+						key3: 'dest3K3',
+						key4: 'dest3K4',
+					})
+					.to(d3);
+
+					pipes.pid4 = dpump.pipe('pid4', {
+						key4: 'dest4K4',
+					})
+					.to(d4);
+
+					// pump
+					return dpump.pump();
+				})
+				.done(function () {
+					// tests
+
+					// d1 remains unaltered.
+					d1.dest1K1.should.eql('v1');
+					should(d1.dest1K2).be.undefined;
+					should(d1.dest1K3).be.undefined;
+					d1.dest1K4.should.eql('v4');
+
+					d2.dest2K1.should.eql('altered-v1');
+					should(d2.dest2K2).be.undefined;
+					d2.dest2K3.should.eql('v3');
+					should(d2.dest2K4).be.undefined;
+
+					d3.dest3K1.should.eql('altered-v1');
+					d3.dest3K2.should.eql('v2');
+					d3.dest3K3.should.eql('v3');
+					d3.dest3K4.should.eql('altered-v4');
+
+
+					should(d4.dest4K1).be.undefined;
+					should(d4.dest4K2).be.undefined;
+					should(d4.dest4K3).be.undefined;
+					d4.dest4K4.should.be.eql('altered-v4');
+
+					testdone();
+				});
+
 		});
 	});
 });
